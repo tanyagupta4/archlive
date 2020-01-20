c895485abf99c4b64b0d97a20d67cec88cf208f3
Anthonymm0994 <42813011+Anthonymm0994@users.noreply.github.com>
2020-01-20 18:39:10 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Dimensions, PixelRatio } from 'react-native';
 import MapView from 'react-native-maps';
 
+const screen = Dimensions.get('window');
+const ASPECT_RATIO = screen.width / screen.height;
+const mapPaddingBottom = screen.heigh * 0.1;
+const mapPaddingTop = screen.height * 0.1;
+
+
 export default class App extends Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <MapView // remove if not using Google Maps
+        <MapView
           style={styles.map}
+          showsTraffic={false}
           region={{
-            latitude: 33.7490,
+            latitude: 33.7500,
             longitude: -84.3880,
             latitudeDelta: 0.030,
             longitudeDelta: 0.024,
-        }}
-      >
+        }}>
      </MapView>
    </View>
     );
@@ -30,4 +37,16 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  border: {
+    borderWidth: 10,
+    borderColor: '#faa',
+    borderBottomWidth: 10,
+    borderBottomColor: '#faa',
+    borderTopWidth: 10,
+    borderTopColor: '#faa',
+    borderLeftWidth: 10,
+    borderLeftColor: '#faa',
+    borderRightWidth: 10,
+    borderRightColor: '#faa',
+  }
  });
