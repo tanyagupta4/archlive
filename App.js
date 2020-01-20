8a486cba7185836df762460e118a47566c9a3683
Anthonymm0994 <42813011+Anthonymm0994@users.noreply.github.com>
2020-01-20 16:20:19 -0500

    Map of Atlanta full Rendered

--- App.js
+++ App.js
@@ -9,10 +9,10 @@ export default class App extends Component {
         <MapView // remove if not using Google Maps
           style={styles.map}
           region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
+            latitude: 33.7490,
+            longitude: -84.3880,
+            latitudeDelta: 0.030,
+            longitudeDelta: 0.024,
         }}
       >
      </MapView>
@@ -24,8 +24,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
