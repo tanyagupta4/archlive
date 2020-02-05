5058655275c6d5c1fa51fe9af12bcae1a96252ee
Pooser, Benjamin Perez <bpooser3@gatech.edu>
2020-02-05 12:15:26 -0500

    added Summerhill info

--- Screens/Information.js
+++ Screens/Information.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import ImageSlider from 'react-native-image-slider';
 
 
 const instructions = Platform.select({
@@ -12,34 +13,51 @@ type Props = {};
 
 export default class Information extends Component<Props> {
   render() {
+    const images = [
+        require('../Images/summerhill_riot.jpg'),
+    ];
      const location = this.props.navigation.getParam('location', 'nothing sent')
     return (
       <View style={styles.container}>
-    <Text> {location} </Text>
+    <Text style={styles.title}>{location} </Text>
+    <ImageSlider
+        style={styles.pic}
+        images={images}
+    />
+    <Text style={styles.info}>Date: 1966 September 6</Text>
+    <Text style={styles.info}> Description: View of part of the crowd gathered in the Summerhill 
+      neighborhood of Atlanta, Georgia during a riot, including people identified 
+      as members of the Student Nonviolent Coordinating Committee speaking into 
+      microphones, while Atlanta Mayor Ivan Allen, Jr. (not pictured) attempts 
+      to lead people to Atlanta Stadium to calm the situation.</Text>
    </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10,
+    color: '#EED000',
+    fontWeight: 'bold',
+    textAlignVertical: 'top',
+  },
+  pic: {
+    marginBottom: 100,
+    width: 400,
+    height: 400,
+    top: 0,
+  },
+  info: {
+    textAlign: 'left',
+    margin: 10,
+    top: -200,
+  },
   container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    flex: 1,
   },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  border: {
-    borderWidth: 10,
-    borderColor: '#faa',
-    borderBottomWidth: 10,
-    borderBottomColor: '#faa',
-    borderTopWidth: 10,
-    borderTopColor: '#faa',
-    borderLeftWidth: 10,
-    borderLeftColor: '#faa',
-    borderRightWidth: 10,
-    borderRightColor: '#faa',
-  }
  });
