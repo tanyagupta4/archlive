0522bb587fe213c5e0918034c41300422620275f
Anthonymm0994 <42813011+Anthonymm0994@users.noreply.github.com>
2020-02-10 12:58:58 -0500

    Updated Home page
    Button is now a touchable. Changes opacity on click and looks cleaner. Should now render the same on all phones screens of different sizes.

--- Screens/home.js
+++ Screens/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button, Image, ImageBackground, Dimensions} from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, Image, ImageBackground, Dimensions, TouchableOpacity} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 const instructions = Platform.select({
@@ -10,10 +10,11 @@ const instructions = Platform.select({
 //might be time to start playing around with these
 const screen = Dimensions.get('window');
 const ASPECT_RATIO = screen.width / screen.height;
-const mapPaddingBottom = screen.height * 0.1;
-const mapPaddingTop = screen.height * 0.1;
+const mapPaddingBottom = screen.height * 0.18;
+const mapPaddingTop = screen.height * 0.11;
+const mapPaddingLeft = screen.width * 0.02;
 
-var width = Dimensions.get("window").width;
+const width = Dimensions.get("window").width;
 
 
 type Props = {};
@@ -23,35 +24,43 @@ export default class HomeScreen extends Component<Props> {
     SplashScreen.hide()
   }
 
+  constructor(props) {
+    super(props);
+    //not using this for now, could implement later
+    //this.state = { myButtonOpacity: 1, };
+  }
+
   render() {
     return (
       <ImageBackground source={require('../Images/home.png')} style={styles.imageBackground}>
-        <View style={styles.hiddentextB}>
-          <Text style={styles.hiddentext}>The Ivan Allen {"\n"}Civil Rights Tour</Text>
+        <View style={styles.transparentTextBackground}>
         </View>
         <View style={styles.textLocation}>
           <Text style={styles.textStyle}>The Ivan Allen {"\n"}Civil Rights Tour</Text>
         </View>
-        <View style={styles.button}>
-          <Button title="Begin" onPress={()=> this.props.navigation.navigate('Map')} />
+        <View style={styles.transparentButtonBackground}>
         </View>
+        <TouchableOpacity style={styles.touchable} activeOpacity={0.3} onPress={()=> this.props.navigation.navigate('Map')}>
+          <Text style={styles.button}>Start</Text>
+        </TouchableOpacity>
       </ImageBackground>
     );
   }
 }
 
+//<Text style={styles.hiddentext}>The Ivan Allen {"\n"}Civil Rights Tour</Text>
+
+// <Button title="Begin" color={'black'} onPress={()=> this.props.navigation.navigate('Map')}/>
+
 const styles = StyleSheet.create({
-  hiddentext: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    color: 'bisque',
-    opacity: 0.0,
-  },
-  hiddentextB: {
+  transparentTextBackground: {
     position: 'absolute',
-    marginTop: 85,
-    backgroundColor: 'gray',
-    opacity: 0.6,
+    marginTop: mapPaddingTop,
+    marginLeft: mapPaddingLeft - 2,
+    height: 100,
+    width: 295,
+    backgroundColor: 'black',
+    opacity: 0.4,
   },
   textStyle: {
     fontSize: 39,
@@ -62,15 +71,38 @@ const styles = StyleSheet.create({
   },
   textLocation: {
     position: 'absolute',
-    marginTop: 85,
+    marginTop: mapPaddingTop,
+    marginLeft: mapPaddingLeft,
   },
   imageBackground: {
     width: '100%',
     height: '100%',
   },
+  touchable: {
+    position: 'absolute',
+    width: 100,
+    height: 50,
+    bottom: mapPaddingBottom,
+    left: width/2 - 50,
+  },
+  transparentButtonBackground: {
+    position: 'absolute',
+    width: 100,
+    height: 50,
+    bottom: mapPaddingBottom,
+    left: width/2 - 50,
+    backgroundColor: 'seashell',
+    opacity: 0.7,
+  },
   button: {
     position: 'absolute',
-    bottom:150,
-    left: 180,
+    top: 3,
+    left: 13,
+    fontSize: 32,
+    fontFamily: 'didot',
+    fontWeight: 'bold',
+    color: 'black',
+    opacity: 0.8,
+    textDecorationLine: 'underline',
   }
 });
