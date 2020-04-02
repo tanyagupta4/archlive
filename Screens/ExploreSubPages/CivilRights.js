4b6461f803a3c1a3def054c40d7986dc94b20b3b
Pooser, Benjamin Perez <bpooser3@gatech.edu>
2020-04-01 21:38:06 -0400

    Completed info screens in Explore

--- Screens/ExploreSubPages/CivilRights.js
+++ Screens/ExploreSubPages/CivilRights.js
@@ -9,12 +9,145 @@ import {
     Image,
     Dimensions,
     TouchableOpacity,
+    Button,
   } from "react-native";
 
-export default class CivilRights extends Component<Props> { 
+export default class CivilRights extends Component<Props> {
+
 
     render() {
-        return (<View></View>)
-    }
+      return (
+          <>
+        <ScrollView scrollEventThrottle={16}>
+        <View style={{ flex: 1, backgroundColor: "white", paddingTop: 50 }}>
+          <Text
+            style={{
+              fontSize: 48,
+              fontWeight: "700",
+              paddingHorizontal: 20,
+              textAlign: 'center',
+              color: "#6e6262"
+            }}
+          >
+            The Civil Rights Act
+          </Text>
+          <Image source={require('../../Images/civilrights2.jpg')} style={{ justifyContent: 'center', alignItems: 'center', width: "100%", height: 300}}/>
+          <Text style={{
+            fontSize: 20,
+            fontWeight: "700",
+            paddingHorizontal: 20,
+            paddingTop: 5,
+            color: 'black',
+            textShadowColor: '#f0eded',
+            textShadowOffset: {width: -1, height: 1},
+            textShadowRadius: 10,
+            textAlign: 'center',
+          }}>
+          The Public Accommodations Bill
+          </Text>
+          <Text style={{
+            paddingTop: 10,
+            paddingHorizontal: 15,
+            color: "#6e6262",
+            fontSize: 14
+          }}>
+          The Public Accommodations Bill states that discrimination of minority groups based on race, color, religion, or national origin is outlawed. When President Kennedy first reached to Allen about testifying for the Public Accommodations Bill, Allen was hesitant. Allen knew how polarized the political climate of Atlanta was, and saw his potential testimony as a serious threat to his chances of winning reelection for mayor, which is seen when Allen writes in his memoir, Mayor; Notes on the Sixties, "Please tell the president what I said and ask him to weigh it. Discuss it with him personally, Morris, and be sure he fully understands all of the angles. And then if calls on me and still wants me to do it, If he recognizes that I can’t be re-elected and that my testimony isn’t going to pass the bill, then I’ll go” (105). Allen believed that this testimony was a sort of self sacrifice of his political career for the Civil Rights Movement, and his willingness to do this shows how deeply Allen cared about the movement. 
+            In July of 1963, Allen gave the testimony in front of a Senate Committee in favor of this bill to end segregation throughout the nation.
+          </Text>
 
+
+          <Text style={{
+            fontSize: 20,
+            fontWeight: "700",
+            paddingHorizontal: 20,
+            paddingTop: 5,
+            color: 'black',
+            textShadowColor: '#f0eded',
+            textShadowOffset: {width: -1, height: 1},
+            textShadowRadius: 10,
+            textAlign: 'center',
+          }}>
+          Allen's Thoughts From Mayor: Notes on the Sixties
+          </Text>
+          <Text style={{
+            paddingTop: 10,
+            paddingHorizontal: 15,
+            color: "#6e6262",
+            fontSize: 14
+          }}>
+          Allen reflected on what it meant to be the Mayor of Atlanta during the Civil Rights Movement when he wrote, “A mayor is right on the scene, easily accessible, and the problems that come to him are immediate. There is sadly little time to theorize or pass the buck to a committee for study… So I had to claw and scratch my way through the civil-rights struggle and learn the hard way” (101). This shows that Allen understood that he had to act on what he thought was right, even if there were negative responses.
+            When Allen decided that he was going to give his testimony on behalf of the Public Accommodations Bill, he knew that he would face a backlash from the Atlanta community and beyond. This is obvious in his memoir, Mayor: Notes on the Sixties, when he recalls himself saying to Morris, “if I go I won't stand a chance of getting re-elected in 'sixty-five. It would be suicide for me to go, and you know it” (105). While Allen anticipated the negative responses that are seen above, he also believed that sacrifices had to be made in order to achieve racial equality.
+            After his testimony, Allen received similar reactions from his friends and family as he received from the strangers in these letters. According to Allen, when he went to North Carolina the weekend following the testimony, he "began to see how deeply the animosity toward the bill and my testimony and myself really ran. I could see my friends that weekend off in the corner by themselves, obviously deeply concerned about what I had done" (114). Allen's family openly criticized his choices which is seen when Allen writes, "Even some of my relatives got into the act, letting it be known that this was the last insult-that there was no way they could deny what Ivan had done this time" (115). While Allen anticipated the backlash from the community as a whole, he did not expect the isolation he felt from his friends and family.
+          </Text>
+
+          <View style ={{
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center'}}>
+
+            <View style={[{ width: 70, margin: 10}]}>
+                <Button color="green" title="some" />
+
+            </View>
+
+
+            <View style={[{ width: 70, margin: 10}]}>
+                <Button color="red" title="More" />
+
+            </View>
+          
+            <View style={[{ width: 70, margin: 10}]}>
+                <Button title="Video" />
+
+            </View>
+
+
+          </View>
+        </View>
+      </ScrollView>
+
+        <View style={styles.fabMenuStyle}>
+            <View>
+                <Button
+            title="CLick to MAP"
+            accessibilityLabel="Go to map to see more"
+            onPress={()=> this.props.navigation.navigate('ExploreMap')}/>
+            </View>
+        </View>
+
+        </>
+    )}
+
+}
+
+const styles = {
+
+  SubmitButtonStyle: {
+    marginTop:15,
+      paddingTop:15,
+      paddingBottom:15,
+      paddingLeft:30,
+      paddingRight:30,
+      backgroundColor:'#6a7fa1',
+      borderRadius:10,
+      borderWidth: 1,
+      borderColor: '#fff'
+},
+
+
+fabMenuStyle: {
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 5,
+    right:0,
+    justifyContent: 'center',
+    alignItems: 'center',
+},
+
+TextStyle:{
+     color:'white',
+     fontWeight: "400",
+     textAlign:'center',
+ },
 }
--- Screens/ExploreSubPages/LesterMaddox.js
+++ Screens/ExploreSubPages/LesterMaddox.js
@@ -9,12 +9,148 @@ import {
     Image,
     Dimensions,
     TouchableOpacity,
+    Button,
   } from "react-native";
 
-export default class LesterMaddox extends Component<Props> { 
+export default class LesterMaddox extends Component<Props> {
+
 
     render() {
-        return (<View></View>)
-    }
+      return (
+          <>
+        <ScrollView scrollEventThrottle={16}>
+        <View style={{ flex: 1, backgroundColor: "white", paddingTop: 50 }}>
+          <Text
+            style={{
+              fontSize: 48,
+              fontWeight: "700",
+              paddingHorizontal: 20,
+              textAlign: 'center',
+              color: "#6e6262"
+            }}
+          >
+            Governor Lester Maddox
+          </Text>
+          <Image source={require('../../Images/lestermaddox2.jpg')} style={{ justifyContent: 'center', alignItems: 'center', width: "100%", height: 400}}/>
+          <Text style={{
+            fontSize: 20,
+            fontWeight: "700",
+            paddingHorizontal: 20,
+            paddingTop: 5,
+            color: 'black',
+            textShadowColor: '#f0eded',
+            textShadowOffset: {width: -1, height: 1},
+            textShadowRadius: 10,
+            textAlign: 'center',
+          }}>
+          </Text>
+          <Text style={{
+            paddingTop: 10,
+            paddingHorizontal: 15,
+            color: "#6e6262",
+            fontSize: 14
+          }}>
+          A defining feature of Ivan Allen Jr.’s term as mayor was his “battles” against Lester Maddox. By portraying Maddox as his nemesis and distancing himself from Maddox’s ideology, Allen was able to solidify his stance as an integrationist and amplify his civil rights rhetoric.
+            Throughout the 1960’s, public clashes between Allen and Maddox were constant. Allen began to take a public stand against Maddox when the two men ran against each other for Atlanta mayor in 1961. His criticism continued beyond 1966 when Maddox won the gubernatorial election, an outcome that was far from ideal in the eyes of Allen. 
+​           By examining the interactions between the two men throughout this decade, one can gain a clearer picture of  Maddox’s racism through its contrast with Allen’s civil rights rhetoric, in addition to understanding the reasons why Maddox was unsuccessful in the election for mayor, but successful in the election for governor.
+          </Text>
 
+
+          <Text style={{
+            fontSize: 20,
+            fontWeight: "700",
+            paddingHorizontal: 20,
+            paddingTop: 5,
+            color: 'black',
+            textShadowColor: '#f0eded',
+            textShadowOffset: {width: -1, height: 1},
+            textShadowRadius: 10,
+            textAlign: 'center',
+          }}>
+          Maddox and Allen: Road to the 1961 Election
+          </Text>
+          <Text style={{
+            paddingTop: 10,
+            paddingHorizontal: 15,
+            color: "#6e6262",
+            fontSize: 14
+          }}>
+          Although their paths crossed as competitors in the 1961 election, the lives that Allen and Maddox led before this encounter were extremely different. Rather than merely comparing the two men, it is interesting to examine their upbringings in relation to their stances on the race issue to understand how they inherited their beliefs from the surroundings they matured in.
+            Ivan Allen was raised in a well-to-do, influential family with great stakes in community affairs. The sense of responsibility he felt for the world around him was evident when through his statement on community engagement in his memoir, Mayor: Notes from the Sixties where he said “The Ivan Allen Company needed me, my family needed me, the Chamber of Commerce needed me.” He makes it a priority to not only carry on the company his father started, but also to be a point of support for other local business owners through the Chamber of Commerce, thus showing his regard for those beyond himself. 
+            In early 1961, when asked about Allen, Constitution editor, Eugene Patterson said that ““He is not content with what he can do for himself; he is concerned with what he can do for people. And that is a pretty noble thing.” Her statement makes it clear that his regard for others was not merely a facade that he put on in his memoir, but rather, it was a genuine interest in helping the community that he exhibited in his daily life. 
+            His upbringing in position of care for the community and engagement with others helped to cultivate his advocacy for civil rights, as it was a means by which he could improve the social and economic situation of Atlanta which he felt that as a prominent resident of the city, was his responsibility.
+            Lester Maddox, on the other hand, was the second of seven children from a non influential Georgia family. He never had strong connections to the business world like Allen did, and instead looked out for himself trying to make ends meet through various blue collar jobs such as when he worked at the Bell Aircraft Factory during World War 2. He was a constant victim of poverty until he and his wife used their life savings to open a restaurant called the Pickrick. He took a strong segregationist stance with his restaurant in the 1960’s, physically pushing African Americans out of the building to make it clear that they were not welcome.
+            Maddox’s upbringing where he was taught to look out for himself and his own interests endowed him with traits that continued into his adulthood. By keeping African Americans separate from whites and viewing them as inferior and dangerous, he was able to stay ahead of them and maintain a perceived sense of superiority. Segregation, in this sense, was a method of self preservation in the eyes of Maddox that was cultivated in him by his upbringing. 
+          </Text>
+
+          <View style ={{
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center'}}>
+
+            <View style={[{ width: 70, margin: 10}]}>
+                <Button color="green" title="some" />
+
+            </View>
+
+
+            <View style={[{ width: 70, margin: 10}]}>
+                <Button color="red" title="More" />
+
+            </View>
+          
+            <View style={[{ width: 70, margin: 10}]}>
+                <Button title="Video" />
+
+            </View>
+
+
+          </View>
+        </View>
+      </ScrollView>
+
+        <View style={styles.fabMenuStyle}>
+            <View>
+                <Button
+            title="CLick to MAP"
+            accessibilityLabel="Go to map to see more"
+            onPress={()=> this.props.navigation.navigate('ExploreMap')}/>
+            </View>
+        </View>
+
+        </>
+    )}
+
+}
+
+const styles = {
+
+  SubmitButtonStyle: {
+    marginTop:15,
+      paddingTop:15,
+      paddingBottom:15,
+      paddingLeft:30,
+      paddingRight:30,
+      backgroundColor:'#6a7fa1',
+      borderRadius:10,
+      borderWidth: 1,
+      borderColor: '#fff'
+},
+
+
+fabMenuStyle: {
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 5,
+    right:0,
+    justifyContent: 'center',
+    alignItems: 'center',
+},
+
+TextStyle:{
+     color:'white',
+     fontWeight: "400",
+     textAlign:'center',
+ },
 }
